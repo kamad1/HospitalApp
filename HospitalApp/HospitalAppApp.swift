1e880f1ac7d84f91d4e6ec96f176dd10e727235c
KaMaDi <artkamadi@gmail.com>
2025-08-07 16:52:20 +0300

    start

--- HospitalApp/HospitalAppApp.swift
+++ HospitalApp/HospitalAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct HospitalAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
