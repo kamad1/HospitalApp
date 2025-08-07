70f398eab1e27d745b0b0775bc33397d7e97f0a5
KaMaDi <artkamadi@gmail.com>
2025-08-07 16:49:24 +0300

    Initial Commit

---
+++ HospitalApp/HospitalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalAppApp.swift
+//  HospitalApp
+//
+//  Created by Jedi on 07.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct HospitalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
